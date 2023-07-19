940885bdcfd8c8a144adf4eeafacd558e37f358a
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-07-19 11:14:54 +0200

    refactor Create separated slices for catalog and cart

--- project/src/store/slices/cart/cartSlice.jsx
+++ project/src/store/slices/cart/cartSlice.jsx
@@ -1,9 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+/**
+ * 
+    "id": 1,
+    "title": "iPhone 9",
+    "description": "An apple mobile which is nothing like apple",
+    "price": 549,
+    "discountPercentage": 12.96,
+    "rating": 4.69,
+    "stock": 94,
+    "brand": "Apple",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    "images": [
+      "https://i.dummyjson.com/data/products/1/1.jpg",
+      "https://i.dummyjson.com/data/products/1/2.jpg",
+      "https://i.dummyjson.com/data/products/1/3.jpg",
+      "https://i.dummyjson.com/data/products/1/4.jpg",
+      "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+    ], 
+    "quantity": 1,
+    "subtotal": 0
+  },
+ */
+
 export const cartSlice = createSlice({
   name: "cart",
   initialState: {
     cartProducts: [],
+    totalCartProducts: 0,
+    isCartModalDisplayed: false,
   },
   reducers: {
     addNewProductToCart: (state, action) => {
@@ -13,34 +39,46 @@ export const cartSlice = createSlice({
         (product) => product.id === newItem.id
       );
       if (foundItem) {
+        // if yes, then change quantity and subtotal
         foundItem.quantity++;
+        foundItem.subtotal = foundItem.subtotal + newItem.price;
       } else {
-        // if not, add it
-        state.cartProducts.push({ ...newItem, quantity: 1 });
+        // if not, add it wih quantity and subtotal new props
+        state.cartProducts.push({
+          ...newItem,
+          quantity: 1,
+          subtotal: newItem.price,
+        });
       }
+      // also, update total of products on cart
+      state.totalCartProducts++;
     },
     removeProductFromCart: (state, action) => {
-      // state.cartProducts = action.payload;
-      const removedItem = action.payload;
-      const foundItem = state.cartProducts.find(
-        (product) => product.id === removedItem.id
-      );
+      //const removedItem = action.payload;
+      const { id, quantity, price } = action.payload;
+      const foundItem = state.cartProducts.find((product) => product.id === id);
       if (foundItem.quantity === 1) {
         // if it is the last item, then remove it
-        state.cartProducts = [state.cartProducts].filter(
-          (product) => product.id !== removedItem.id
+        state.cartProducts = state.cartProducts.filter(
+          (product) => product.id !== id
         );
+        foundItem.quantity = foundItem.quantity - quantity;
       } else {
-        // if not, change its quantity
         foundItem.quantity--;
+        foundItem.subtotal = foundItem.subtotal - price;
       }
+      state.totalCartProducts--;
     },
-
     deleteProductFromCart: (state, action) => {
-      const deletedItemId = action.payload;
-      state.cartProducts = [state.cartProducts].filter(
-        (product) => product.id !== deletedItemId
+      //const deletedItem = action.payload;
+      const { id, quantity } = action.payload;
+      state.cartProducts = state.cartProducts.filter(
+        (product) => product.id !== id
       );
+      state.totalCartProducts = state.totalCartProducts - quantity;
+    },
+    manageCartModalDisplay: (state) => {
+      state.isCartModalDisplayed = !state.isCartModalDisplayed;
     },
   },
 });
@@ -49,4 +87,5 @@ export const {
   addNewProductToCart,
   removeProductFromCart,
   deleteProductFromCart,
+  manageCartModalDisplay,
 } = cartSlice.actions;
--- project/src/store/slices/catalog/catalogSlice.jsx
+++ project/src/store/slices/catalog/catalogSlice.jsx
@@ -5,6 +5,18 @@ export const loadProductsData = createAsyncThunk(
   "catalog/loadProducts",
   async () => {
     const response = await api.get("/products");
+    const data = response.data.map((product) => {
+      const obj = { ...product, isFavourite: false };
+      return obj;
+    });
+    return data;
+  }
+);
+
+export const loadProductData = createAsyncThunk(
+  "catalog/loadProduct",
+  async (productId) => {
+    const response = await api.get(`/products/${productId}`);
     return response.data;
   }
 );
@@ -13,10 +25,27 @@ export const catalogSlice = createSlice({
   name: "catalog",
   initialState: {
     catalogProducts: [],
+    product: [],
+    favourites: [],
     isDataLoading: false,
     hasError: false,
   },
-  reducers: {},
+  reducers: {
+    manageFavouriteItem: (state, action) => {
+      const id = action.payload;
+      // change prop to true
+      const itemIndex = state.catalogProducts.findIndex(
+        (product) => product?.id === id
+      );
+      state.catalogProducts[itemIndex].isFavourite = true;
+      // filter the favorites
+      const favouriteList = state.catalogProducts.filter(
+        (product) => product.isFavourite === true
+      );
+      // save into state
+      state.favourites = favouriteList;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(loadProductsData.pending, (state) => {
       state.isDataLoading = true;
@@ -25,5 +54,15 @@ export const catalogSlice = createSlice({
       state.catalogProducts = action.payload;
       state.isDataLoading = false;
     });
+    builder.addCase(loadProductData.pending, (state) => {
+      state.product = [];
+      state.isDataLoading = true;
+    });
+    builder.addCase(loadProductData.fulfilled, (state, action) => {
+      state.product = action.payload;
+      state.isDataLoading = false;
+    });
   },
 });
+
+export const { manageFavouriteItem } = catalogSlice.actions;
